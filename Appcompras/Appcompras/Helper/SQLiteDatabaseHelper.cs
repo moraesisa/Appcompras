f38371cba516182981c5f7586a6631e13b3fa159
Usuario <Usuario@CPD03-M07>
2022-11-23 13:47:10 -0300

    Adicionar arquivos de projeto.

---
+++ Appcompras/Appcompras/Helper/SQLiteDatabaseHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Appcompras.Model;
+using SQLite;
+
+namespace Appcompras.Helper
+{
+    public class SQLiteDatabaseHelper
+    {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+        public Task<int> insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+
+        public void update(Produto p)
+        {
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE id= ?";
+
+            _conn.QueryAsync<Produto>(sql, p.Descricao, p.Preco, p.ID);
+        }
+
+      
+        public Task<List<Produto>> getAll()
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public void delete(int Id)
+        {
+            _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+    }
+}
---
+++ Appcompras/Appcompras/View/NovoProduto.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Appcompras.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Appcompras.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NovoProduto : ContentPage
+    {
+        public NovoProduto()
+        {
+            InitializeComponent();
+        }
+
+        private async  void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Produto P = new Produto
+                {
+                    Descricao = txt_descricao.Text,
+                    Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                    Preco = Convert.ToDouble(txt_preco.Text),
+                };
+
+                App.Database.Insert(P);
+
+                //Navigation.PushAsync(new NovoProduto());
+
+            }
+            catch (Exception ex)
+
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+    }
+}
